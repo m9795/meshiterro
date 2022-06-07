73c17f18b510ca75124e05cebc98d4c636a0ba77
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-07 09:59:04 +0900

    [Add] 4-15マイページの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
-has_many :post_images, dependent: :destroy
-
-has_one_attached :profile_image
+  has_many :post_images, dependent: :destroy
+  
+  has_one_attached :profile_image
 
   def get_profile_image
     unless profile_image.attached?
